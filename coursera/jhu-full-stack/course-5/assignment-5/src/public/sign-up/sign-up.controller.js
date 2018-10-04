97b0121552331745694825270f806c68af4ab2b7
Robert DiMartino <robert.dimartino@adhocteam.us>
2018-10-04 11:49:54 -0400

    assignment 5 fix

--- coursera/jhu-full-stack/course-5/assignment-5/src/public/sign-up/sign-up.controller.js
+++ coursera/jhu-full-stack/course-5/assignment-5/src/public/sign-up/sign-up.controller.js
@@ -3,15 +3,15 @@
 
   angular.module("public").controller("SignUpController", SignUpController);
 
-  SignUpController.$inject = ["UserInfoService", "MenuService"];
-  function SignUpController(UserInfoService, MenuService) {
+  SignUpController.$inject = ["UserInfoService", "MenuService", "$scope"];
+  function SignUpController(UserInfoService, MenuService, $scope) {
     var $ctrl = this;
     $ctrl.userInfo = UserInfoService.getInfo();
     $ctrl.info_saved = false;
-    $ctrl.invalid_favorite = false;
+    $ctrl.invalid_favorites = new Set();
+    $scope.isInvalidFavorite = false;
 
     $ctrl.submit = function() {
-      UserInfoService.setInfo($ctrl.userInfo);
       MenuService.getMenuItems().then(function(result) {
         var invalidFavorite = false;
         try {
@@ -23,9 +23,22 @@
           console.error(err);
           invalidFavorite = true;
         }
-        $ctrl.invalid_favorite = invalidFavorite;
-        $ctrl.info_saved = true;
+        if (invalidFavorite) {
+          $ctrl.invalid_favorites.add($ctrl.userInfo.favorite_dish);
+        }
+        if ($ctrl.checkInvalidFavorite()) {
+          $ctrl.info_saved = false;
+        } else {
+          UserInfoService.setInfo($ctrl.userInfo);
+          $ctrl.info_saved = true;
+        }
       });
+    };
+
+    $ctrl.checkInvalidFavorite = function() {
+      var invalid = $ctrl.invalid_favorites.has($ctrl.userInfo.favorite_dish);
+      $scope.isInvalidFavorite = invalid;
+      return invalid;
     };
   }
 })();
